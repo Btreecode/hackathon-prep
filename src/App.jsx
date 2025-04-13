a9be35d9977a06aaf6ec31b1efd5eadec5294b4d
btreecode2 <btreecode2@gmail.com>
2025-04-13 00:03:08 +0000

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <div className="font-bold text-blue-600">123123</div>
+      <div>123123</div>
+      <div>123123</div>
+    </div>
+  )
+}
+
+export default App
